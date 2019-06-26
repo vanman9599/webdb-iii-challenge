1e70497027eeb99b1234740375e07556f7a9f777
van jordan <vanjordan46@hotmail.com>
2019-06-26 15:33:42 -0700

    Almost done?

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development);
+const server = express();
+server.use(helmet());
+server.use(express.json());
+
+server.get('/api/cohorts', async (req, res) => {
+    // get the cohorts from the database
+    try {
+      const cohorts = await db('cohorts'); // all the records from the table
+      res.status(200).json(cohorts);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+  // list a role by id
+server.get('/api/cohorts/:id', async (req, res) => {
+    // get the cohorts from the database
+    try {
+      const cohort = await db('cohorts')
+        .where({ id: req.params.id })
+        .first();
+      res.status(200).json(cohort);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+  
+  const errors = {
+    '19': 'Another record with that value exists',
+  };
+  
+  // create cohorts
+  server.post('/api/cohorts', async (req, res) => {
+    try {
+      const [id] = await db('cohorts').insert(req.body);
+  
+      const cohort = await db('cohorts')
+        .where({ id })
+        .first();
+  
+      res.status(201).json(cohort);
+    } catch (error) {
+      const message = errors[error.errno] || 'We ran into an error';
+      res.status(500).json({ message, error });
+    }
+  });
+  // update cohorts
+  server.put('/api/cohorts/:id', async (req, res) => {
+    try {
+      const count = await db('cohorts')
+        .where({ id: req.params.id })
+        .update(req.body);
+  
+      if (count > 0) {
+        const cohort = await db('cohorts')
+          .where({ id: req.params.id })
+          .first();
+  
+        res.status(200).json(cohort);
+      } else {
+        res.status(404).json({ message: 'Records not found' });
+      }
+    } catch (error) {}
+  });
+  
+  // remove cohorts (inactivate the role)
+  server.delete('/api/cohorts/:id', async (req, res) => {
+    try {
+      const count = await db('cohorts')
+        .where({ id: req.params.id })
+        .del();
+  
+      if (count > 0) {
+        res.status(204).end();
+      } else {
+        res.status(404).json({ message: 'Records not found' });
+      }
+    } catch (error) {}
+  });
+
+
+
+const port = process.env.PORT || 3001;
+server.listen(port, () => 
+    console.log(`\n** API running pon http://localhost:${port} **\n`)
+);
